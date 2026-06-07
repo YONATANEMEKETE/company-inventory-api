ddeebe1e21d632038e0cbbb66201c041a6525a59
YONATANEMEKETE <yonatanemekete33@gmail.com>
2026-06-07 11:38:22 +0300

    install prisma and set it up all

---
+++ src/shared/db/prisma.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { config } from '../configs/env.js';
+
+const adapter = new PrismaPg({ connectionString: config.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
