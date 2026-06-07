62f1ac37942ec8240a374cd53be3f4bb1d79a6f4
YONATANEMEKETE <yonatanemekete33@gmail.com>
2026-06-07 12:31:20 +0300

    installed session libs and mounted the session middleware and add session model to the schema

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,39 @@ import express from 'express';
 import { requestLogger } from './shared/middlewares/requets-logger.js';
 import { notFoundHandler } from './shared/middlewares/notfound-handler.js';
 import { errorHandler } from './shared/middlewares/error-handler.js';
+import session from 'express-session';
+import pgSession from 'connect-pg-simple';
+import pg from 'pg';
+import { config } from './shared/configs/env.js';
+
+const PostgresStore = pgSession(session);
+const pool = new pg.Pool({
+  connectionString: config.DATABASE_URL,
+});
 
 export const app = express();
 
 app.use(requestLogger);
 app.use(express.json());
+app.use(
+  session({
+    store: new PostgresStore({
+      pool,
+      tableName: 'session',
+      createTableIfMissing: false,
+    }),
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    name: 'sid',
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: config.NODE_ENV === 'production',
+    },
+  }),
+);
 
 app.get('/health', (req, res) => {
   res.send('Server is running');
